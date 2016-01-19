a30f15ddd25a0e8e006548e6427872d1e390ab7c
knutux <knutux@gmail.com>
2016-01-19 12:22:11 +0200

    version 0.7.0 - village name cleanup and some small additions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,16 @@ CHANGELOG OpenTTD Lithuanian Town Names
 ========================================================================
 ------------------------------------------------------------------------
 
+v0.7.0 (2016-01-19)
+-------------------
+
+- Village name cleanup and some additional small town names
+
+v0.6.0 (2016-01-19)
+-------------------
+
+- Name change (adding "More" to distinguish from older GRF of Lithuanian town names)
+
 v0.5.0 (2016-01-19)
 -------------------
 
--- utils/extractTownNames.r
+++ utils/extractTownNames.r
@@ -10,51 +10,71 @@ writeUtf8 <- function(lines, file, bom=F) {
     close(con)
 }
 
-extractTownNames <- function (fileName)
+extractNamesFromCategory <- function (category)
 {
-    library (httr)
-
-    nextPage <- ""
-    baseUrl <- 'https://lt.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Kategorija:Nepilni_(Lietuvos_gyvenviet%C4%97s)&format=json&cmtype=page&cmlimit=100'
+    cachedFileName = paste0(category,'.list');
+    if (file.exists(cachedFileName))
+        return (load(cachedFileName))
+        
+    baseUrl <- paste0('https://lt.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Kategorija:',category,'&format=json&cmtype=page&cmlimit=100')
     pageList <- list()
     cnt <- 0
+    nextPage <- ""
 
     while (cnt < 1000)
-        {
+    {
         cnt <- cnt + 1
-
+        
         if (nextPage != "")
-            {
+        {
             url <- paste (baseUrl, "&cmcontinue=", nextPage, sep='')
-            }
+        }
         else
             url <- baseUrl
-
+        
         response <- GET (url)
         if (is.null(response))
             stop (paste('Could not fetch from', url))
-    
+        
         json <- content(response)
         json <- jsonlite::fromJSON(jsonlite::toJSON(json))
-
+        
         if (json$batchcomplete != "")
             break
-
+        
         nextPage <- json$continue$cmcontinue
         pageNames <- json$query$categorymembers$title
         print (paste('fetched', length(pageNames), 'page names'))
 
-        m <- sub(' \\(.+\\)', '', pageNames, perl=TRUE)
-        pageList <- c(pageList, m)
+        pageList <- c(pageList, pageNames)
         if (length(pageNames)< 100)
-            {
-            print(json)
+        {
+            #print(json)
             break
-            }
-
+        }
+        
         Sys.sleep(1)
     }
 
+    save (pageList,file=cachedFileName)
+    pageList
+}
+
+extractTownNames <- function (fileName)
+{
+    library (httr)
+
+    incomplete <- extractNamesFromCategory ('Nepilni_(Lietuvos_gyvenviet%C4%97s)')
+    towns <- extractNamesFromCategory ('Lietuvos_miesteliai')
+    pageList <- c(incomplete, towns)
+    print (paste('Extracted', length(incomplete), 'village and ', length(towns), 'town names. Total -', length(incomplete)+length(towns)))
+    pageList <- sub(' \\(.+\\)', '', pageList, perl=TRUE)
+    pageList <- sub(' [I]+$', '', pageList, perl=TRUE)
+    pageList <- sub('u GS$', 'ai', pageList, perl=TRUE)
+    pageList <- sub('es GS$', 'e', pageList, perl=TRUE)
+    pageList <- sub('io GS$', 'is', pageList, perl=TRUE)
+    pageList <- sub('o GS$', 'as', pageList, perl=TRUE)
+    
     townNames <- unlist(unique (pageList))
 
     print (paste('Writing', length(townNames), 'names to a file', fileName))
